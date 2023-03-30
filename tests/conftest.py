35af24f8dbba085cd2d74b862ba3011e7de25629
Monica Cornescu <cornescu.monica@gmail.com>
2023-03-30 20:58:41 +0200

    modify fixture to run tests for both browsers. Also install pytest-xdist package to run multiple tests in parallel using pycharm configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
+"""
+This file contains setup functions.
+The driver function is initially created to take --browser argument passed at runtime.
+Then we modify it to take the 2 browsers as parameters of the fixture
+"""
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,9 +12,23 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-@pytest.fixture()
+# @pytest.fixture()
+# def driver(request):
+#     browser = request.config.getoption("--browser")
+#     print(f"Creating {browser} driver")
+#     if browser == "chrome":
+#         used_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#     elif browser == "firefox":
+#         used_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+#     else:
+#         raise TypeError(f'Expected "chrome" or "firefox", but got {browser}')
+#     yield used_driver
+#     print(f"Closing {browser} driver")
+#     used_driver.quit()
+
+@pytest.fixture(params=["chrome", "firefox"])
 def driver(request):
-    browser = request.config.getoption("--browser")
+    browser = request.param
     print(f"Creating {browser} driver")
     if browser == "chrome":
         used_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
