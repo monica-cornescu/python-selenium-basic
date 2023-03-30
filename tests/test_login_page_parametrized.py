c84bb968c79dba50159c52e263788c2bc53e6d23
Monica Cornescu <cornescu.monica@gmail.com>
2023-03-30 12:12:27 +0200

    test negative scenarios with parametrized mark in new test file

---
+++ tests/test_login_page_parametrized.py
@@ -0,0 +1,36 @@
+import time
+import pytest
+from selenium.webdriver.common.by import By
+
+
+class TestNegativeScenarios:
+
+    @pytest.mark.login
+    @pytest.mark.negative
+    @pytest.mark.parametrize("username, password, expected_error_message",
+                             [("incorrectUser", "Password123", "Your username is invalid!"),
+                              ("student", "incorrectPassword", "Your password is invalid!")])
+    def test_negative_username(self, driver, username, password, expected_error_message):
+        # Go to webpage
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # Type username incorrectUser into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys(username)
+
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys(password)
+
+        # Press Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed but it should be!"
+
+        # Verify error message text is Your username is invalid!
+        error_message = error_message_locator.text
+        assert error_message == expected_error_message, 'Did not expect this error message!'
