e982adef1d6374532adbae750144aab65c3bb152
Monica Cornescu <cornescu.monica@gmail.com>
2023-04-08 12:37:14 +0200

    improve base object page and make login_page inherit from it

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -3,29 +3,37 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as expcond
 
+from page_objects.base_page import BasePage
 
-class LoginPage:
+
+class LoginPage(BasePage):
     __url = "https://practicetestautomation.com/practice-test-login/"
     __username_field = (By.ID, "username")
     __password_field = (By.NAME, "password")
     __submit_button = (By.XPATH, "//button[@class='btn']")
 
     def __init__(self, driver: WebDriver):
-        self._driver = driver
+        super().__init__(driver)
 
-    def open(self):
-        self._driver.get(self.__url)
+    def open(self, url: str = __url):
+        super().open_url(self.__url)
+
+    # def execute_login(self, username: str, password: str):   # upgrading this to use parent class BasePage
+    #     # Type the username in its location after checking for visibility of element
+    #     wait = WebDriverWait(self._driver, 6)
+    #     wait.until(expcond.visibility_of_element_located(self.__username_field))
+    #     self._driver.find_element(self.__username_field).send_keys(username)
+    #
+    #     # Type password in its location after checking for visibility of element
+    #     wait.until(expcond.visibility_of_element_located(self.__password_field))
+    #     self._driver.find_element(self.__password_field).send_keys(password)
+    #
+    #     # Press Submit button after checking for visibility of element
+    #     wait.until(expcond.visibility_of_element_located(self.__submit_button))
+    #     self._driver.find_element(self.__submit_button).click()
 
     def execute_login(self, username: str, password: str):
-        # Type the username in its location after checking for visibility of element
-        wait = WebDriverWait(self._driver, 6)
-        wait.until(expcond.visibility_of_element_located(self.__username_field))
-        self._driver.find_element(self.__username_field).send_keys(username)
+        super()._insert_text(self.__username_field, username)
+        super()._insert_text(self.__password_field, password)
+        super()._click(self.__submit_button)
 
-        # Type password in its location after checking for visibility of element
-        wait.until(expcond.visibility_of_element_located(self.__password_field))
-        self._driver.find_element(self.__password_field).send_keys(password)
-
-        # Press Submit button after checking for visibility of element
-        wait.until(expcond.visibility_of_element_located(self.__submit_button))
-        self._driver.find_element(self.__submit_button).click()
